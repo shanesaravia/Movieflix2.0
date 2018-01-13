4ff1d21129659ce99136b7a2d6dd39bb2c38a522
admin <shanesaravia@live.ca>
2018-01-13 03:05:54 -0500

    adding django base files.

--- myproject/settings.py
+++ myproject/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u@@sl&-vg%%^zdps6c55*)z_y6^jed*50ae)ycgjgps89jfc-2'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
