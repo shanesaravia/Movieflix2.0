3c24b61ee6c6b2fa03762254bf902add3b85d548
Shane Saravia <shanesaravia@live.ca>
2018-05-22 21:54:41 -0400

    adjusted settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -81,11 +81,11 @@ if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ['DB_NAME'],
-            'USER': os.environ['DB_USER'],
-            'PASSWORD': os.environ['DB_PASSWORD'],
-            'HOST': os.environ['DB_HOST'],
-            'PORT': os.environ['DB_PORT']
+            'NAME': os.environ['RDS_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT']
         }
     }
 
