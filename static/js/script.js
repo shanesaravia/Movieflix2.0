ac475fb76acc4cdee26c4c126461add9a5cc46db
Shane Saravia <shanesaravia@live.ca>
2018-05-28 21:42:32 -0400

    cleaned up js

--- static/js/script.js
+++ static/js/script.js
@@ -24,9 +24,7 @@ window.onload = function() {
 	document.getElementById("search").value = '';
 }
 
+// Clear search filter button
 document.getElementById('clear-filter-btn').addEventListener("click", function() {
 	window.location.href = window.location.href.split('results/?')[0];
 })
-// $("#cle").click(function(){
-//     window.location.href = window.location.href.split('?')[0];
-// });
